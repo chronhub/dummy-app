d0d16308435a2b94fc307009ceb94a2aa08f5d17
steph bug <chronhubgit@gmail.com>
2024-01-31 09:43:03 +0100

    wip

--- app/Chron/Domain/Command/RegisterCustomer.php
+++ app/Chron/Domain/Command/RegisterCustomer.php
@@ -4,18 +4,9 @@ declare(strict_types=1);
 
 namespace App\Chron\Domain\Command;
 
-use App\Chron\Attribute\Validating\AsValidation;
 use Storm\Contract\Message\DomainCommand;
 use Storm\Message\AbstractDomainCommand;
 
-#[AsValidation(
-    rules: [
-        'customer_id' => 'required|string|uuid',
-        'customer_name' => 'required|string|between:3,255|regex:/^[a-zA-Z0-9\s]+$/',
-        'customer_email' => 'required|email',
-    ],
-    beforeDispatch: true,
-)]
 final class RegisterCustomer extends AbstractDomainCommand
 {
     public static function with(string $id, string $name, string $email): DomainCommand
