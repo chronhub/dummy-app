c0a27e7b120205c9825c203c580b63619ae0cec6
steph bug <chronhubgit@gmail.com>
2024-03-18 14:33:53 +0100

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,6 +17,7 @@ use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 use Storm\Contract\Message\MessageFactory;
 use Storm\Message\MessageServiceProvider;
+use Storm\Support\Providers\StormServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,22 +27,23 @@ class AppServiceProvider extends ServiceProvider
 
     public function register(): void
     {
-        $this->app->register(ClockServiceProvider::class);
-        $this->app->register(KernelServiceProvider::class);
-        $this->app->register(MessageServiceProvider::class);
-        $this->app->register(ReporterServiceProvider::class);
-        $this->app->register(ChroniclerServiceProvider::class);
+//        $this->app->register(ClockServiceProvider::class);
+//        $this->app->register(KernelServiceProvider::class);
+//        $this->app->register(MessageServiceProvider::class);
+//        $this->app->register(ReporterServiceProvider::class);
+//        $this->app->register(ChroniclerServiceProvider::class);
+        $this->app->register(StormServiceProvider::class);
         $this->app->register(ShopServiceProvider::class);
 
         // to message service provider
-        $this->app->alias(MessageFactory::class, 'message.factory.default');
-        $this->app->bind('message.decorator.chain.default', function (Application $app) {
-            return new ChainMessageDecorator(
-                new EventId(),
-                new EventType(),
-                $app[EventTime::class],
-                new EventDispatched()
-            );
-        });
+//        $this->app->alias(MessageFactory::class, 'message.factory.default');
+//        $this->app->bind('message.decorator.chain.default', function (Application $app) {
+//            return new ChainMessageDecorator(
+//                new EventId(),
+//                new EventType(),
+//                $app[EventTime::class],
+//                new EventDispatched()
+//            );
+//        });
     }
 }
